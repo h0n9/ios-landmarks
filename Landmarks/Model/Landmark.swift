66026bae07dc4bea64f777fc1b73f93a4413c7fc
Jaehong Ahn <monsieur.ahn@gmail.com>
2022-02-16 07:12:07 +0900

    Add ModelData, Landmark swift files
    - reorganize file struct
    - add lots of image assets

---
+++ Landmarks/Model/Landmark.swift
@@ -0,0 +1,39 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by h0n9 on 2022/02/16.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Coordinates: Hashable, Codable {
+    var latitude: Double
+    var longitude: Double
+}
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    // read from json file but unused in public
+    private var imageName: String
+    private var coordinates: Coordinates
+    
+    // init from private vars and used in public
+    var image: Image {
+        Image(imageName)
+    }
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude
+        )
+    }
+}
+
