92d12285d922dbeb5d94f20d9a12d6e661dfdd08
Jaehong Ahn <monsieur.ahn@gmail.com>
2022-02-16 06:48:48 +0900

    Add MapView

---
+++ Landmarks/Map.swift
@@ -0,0 +1,32 @@
+//
+//  Map.swift
+//  Landmarks
+//
+//  Created by h0n9 on 2022/02/16.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    // @State attribute can be modified from more than one view
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(
+            latitude: 34.011_286,
+            longitude: -116.166_868
+        ),
+        span: MKCoordinateSpan(
+            latitudeDelta: 0.2,
+            longitudeDelta: 0.2
+        )
+    )
+    var body: some View {
+        Map(coordinateRegion: $region)
+    }
+}
+
+struct Map_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
