4af0a493745d43affeacd60f2d63902b59a71577
Jaehong Ahn <monsieur.ahn@gmail.com>
2022-02-15 21:31:25 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by h0n9 on 2022/02/15.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
